8fc795dc10338efbff785613000d2e624eaeb76b
AbhishekRaoV <abhishekraovelichala@gmail.com>
2023-12-12 05:37:09 +0000

    updates

--- binarytree.py
+++ binarytree.py
@@ -1,31 +1,17 @@
 class TreeNode:
     def __init__(self, value):
-        """
-        Initialize a new TreeNode with the given value.
-        """
         self.value = value
         self.left = None
         self.right = None
 
-
 class BinaryTree:
     def __init__(self):
-        """
-        Initialize a new empty BinaryTree.
-        """
         self.root = None
 
     def insert(self, value):
-        """
-        Insert a new node with the given value into the binary tree.
-        Raise ValueError if the value already exists in the tree.
-        """
         self.root = self._insert(self.root, value)
 
     def _insert(self, node, value):
-        """
-        Helper method to recursively insert a new node with the given value into the binary tree.
-        """
         if node is None:
             return TreeNode(value)
         if value < node.value:
@@ -33,20 +19,14 @@ class BinaryTree:
         elif value > node.value:
             node.right = self._insert(node.right, value)
         else:
+            # Value already exists, raise ValueError
             raise ValueError("Value already exists in the tree")
         return node
 
     def search(self, value):
-        """
-        Search for a node with the given value in the binary tree.
-        Return the node if found, otherwise return None.
-        """
         return self._search(self.root, value)
 
     def _search(self, node, value):
-        """
-        Helper method to recursively search for a node with the given value in the binary tree.
-        """
         if node is None or node.value == value:
             return node
         if value < node.value:
@@ -54,8 +34,8 @@ class BinaryTree:
         return self._search(node.right, value)
 
 
+# Test cases using unittest
 import unittest
-
 
 class TestBinaryTree(unittest.TestCase):
     def setUp(self):
@@ -75,7 +55,6 @@ class TestBinaryTree(unittest.TestCase):
     def test_search(self):
         self.assertIsNotNone(self.tree.search(3))
         self.assertIsNone(self.tree.search(8))
-
 
 if __name__ == '__main__':
     unittest.main()
